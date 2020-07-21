bb0f36e408698d12f312194f284432d965517eba
ahmetfarukulu <ahmetfarukulu@gmail.com>
2020-07-21 13:07:39 +0300

    NinjectControllerFactory eklendi Log testi yapıldı

--- DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -22,6 +22,7 @@ namespace DevFramework.Northwind.Business.Concrete.Managers
 
         [FluentValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        [LogAspect(typeof(FileLogger))]
         public Product Add(Product product)
         {
             return _productDal.Add(product);
