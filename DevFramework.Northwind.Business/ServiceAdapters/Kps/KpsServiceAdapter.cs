c9b57dcc4deb3e801e29e969f9aade092e947e32
ahmetfarukulu <ahmetfarukulu@gmail.com>
2020-07-24 14:02:29 +0300

    Businessa servis adaptör katmanı eklendi

---
+++ DevFramework.Northwind.Business/ServiceAdapters/Kps/KpsServiceAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.ServiceAdapters.Kps
+{
+    public class KpsServiceAdapter : IKpsService
+    {
+        public bool Validate(object Data)
+        {
+            //Servis örneği oluşturulur 
+            //Gerekli metot çağırılır
+            return true;
+        }
+    }
+}
---
+++ DevFramework.Northwind.Business/ServiceAdapters/Kps/TestKpsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.ServiceAdapters.Kps
+{
+    public class TestKpsService : IKpsService
+    {
+        public bool Validate(object Data)
+        {
+            return true;
+        }
+    }
+}
